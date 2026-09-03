bcb8b721542ea1272e3581ec45d467b458734482
RahoniCouto <rahonidevcouto@gmail.com>
2026-09-03 00:59:40 -0300

    constrain editorial hero media sizing

--- build/blocks/editorial-hero/index.asset.php
+++ build/blocks/editorial-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => '28d740c557481566ef58');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-html-entities', 'wp-i18n'), 'version' => 'da3ba038affec53047bf');
